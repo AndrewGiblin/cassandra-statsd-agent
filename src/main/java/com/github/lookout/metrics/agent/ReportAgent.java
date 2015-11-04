79f41fb2ea32dd14a724d802b1d902e1052f49e1
dev <dev@localhost.local>
2015-11-04 14:13:17 -0800

    Added support to configure a prefix, based on code submitted by https://github.com/petere/cassandra-statsd-agent

--- src/main/java/com/github/lookout/metrics/agent/ReportAgent.java
+++ src/main/java/com/github/lookout/metrics/agent/ReportAgent.java
@@ -12,11 +12,12 @@ import java.util.concurrent.TimeUnit;
 public class ReportAgent {
 
     public static void premain(final String agentArgs, final Instrumentation inst) {
-        final String prefix = "cassandra";
+        String prefix = "cassandra";
 
         final String[] reportingHostPorts = (agentArgs != null) ? agentArgs.split(",") : new String[]{null};
         for (final String reportingHostPort : reportingHostPorts) {
             final HostPortInterval hostPortInterval = new HostPortInterval(reportingHostPort);
+            prefix = hostPortInterval.getPrefix() != null ? hostPortInterval.getPrefix() : prefix;
             final StatsDClient client = new NonBlockingStatsDClient(prefix, hostPortInterval.getHost(), hostPortInterval.getPort());
             final StatsdReporter reporter = new StatsdReporter(hostPortInterval, client);
             reporter.start(hostPortInterval.getInterval(), TimeUnit.SECONDS);
